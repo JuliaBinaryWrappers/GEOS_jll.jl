a3ac59e1c917e33402c321a6d815cac8eda68ff0
jlbuild <juliabuildbot@gmail.com>
2024-05-25 19:22:08 +0000

    GEOS_jll build 3.12.1+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,19 +3,19 @@ export geosop, libgeos, libgeos_cpp
 
 JLLWrappers.@generate_wrapper_header("GEOS")
 JLLWrappers.@declare_library_product(libgeos, "@rpath/libgeos_c.1.dylib")
-JLLWrappers.@declare_library_product(libgeos_cpp, "@rpath/libgeos.3.12.0.dylib")
+JLLWrappers.@declare_library_product(libgeos_cpp, "@rpath/libgeos.3.12.1.dylib")
 JLLWrappers.@declare_executable_product(geosop)
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libgeos,
-        "lib/libgeos_c.1.18.0.dylib",
+        "lib/libgeos_c.1.18.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libgeos_cpp,
-        "lib/libgeos.3.12.0.dylib",
+        "lib/libgeos.3.12.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
